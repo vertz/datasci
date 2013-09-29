39b2f7603efe527c744135d12df852d16e36da25
vertz <winter333@gmail.com>
2013-09-29 13:26:14 +0200

    Update multiply.py

--- assignment3/multiply.py
+++ assignment3/multiply.py
@@ -2,7 +2,22 @@ import MapReduce
 import sys
 
 """
-Word Count Example in the Simple Python MapReduce Framework
+Assume you have two matrices A and B in a sparse matrix format, 
+where each record is of the form i, j, value.  
+
+Design a MapReduce algorithm to compute matrix multiplication: A x B
+
+The input to the map function will be matrix row records formatted as lists. 
+Each list will have the format [matrix, i, j, value] 
+where matrix is a string and i, j, and value are integers.
+
+The first item, matrix, is a string that identifies which matrix the record originates from. 
+This field has two possible values:
+    ‘a’ indicates that the record is from matrix A
+    ‘b’ indicates that the record is from matrix B
+    
+The output from the reduce function will also be matrix row records formatted as tuples. 
+Each tuple will have the format (i, j, value) where each element is an integer.
 """
 
 mr = MapReduce.MapReduce()
