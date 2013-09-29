f86f147feae173b5061a2bed848de905ccb4ee81
vertz <winter333@gmail.com>
2013-09-29 13:30:12 +0200

    Update join.py

--- assignment3/join.py
+++ assignment3/join.py
@@ -12,6 +12,19 @@ MapReduce query should produce the same information as this SQL query.
 
 consider the two input tables, Order and LineItem, as one big concatenated bag of records 
 which gets fed into the map function record by record
+
+The input will be database records formatted as lists of Strings.
+
+Every list element corresponds to a different field in it’s corresponding record.
+The first item(index 0) in each record is a string that identifies which table the record originates from. 
+This field has two possible values:
+
+   ‘line_item’ indicates that the record is a line item.
+   ‘order’ indicates that the record is an order.
+
+The second element(index 1) in each record is the order_id.
+
+The output should be a joined record.
 """
 
 mr = MapReduce.MapReduce()
